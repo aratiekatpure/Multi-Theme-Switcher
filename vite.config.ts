6731f2f0e2809844e0ddc2dc778c99ab6a93ea5a
aratiekatpure <ekatpurearati18@gmail.com>
2025-08-22 10:59:07 +0530

    Deploy build to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Multi-Theme-Switcher/",
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
